ef0336bac4d3984b505321223a7b1e7e163ac6ba
clintonium-119 <clint.beacock@gmail.com>
2025-12-08 21:26:53 -0500

    Pane navigation in zellij working

---
+++ private_dot_config/nvim/lua/plugins/smart-splits.lua
@@ -0,0 +1,4 @@
+-- Disabled: using zellij-nav.nvim instead for Zellij integration
+-- smart-splits requires Zellij to detect nvim which doesn't work when
+-- running nvim through opencode (command shows 'opencode' not 'nvim')
+return {}
